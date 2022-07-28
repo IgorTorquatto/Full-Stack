7818cb34b77a29f34ddc2e371a7b0475603ac538
Igor <igor.torquato.20@gmail.com>
2022-07-28 19:12:59 -0300

    Novos Arquivos

---
+++ Códigos/Javascript/Exercicios/Exercicio 1/script.js
@@ -0,0 +1,5 @@
+alert("Bem-vindo a seguir pediremos que informe alguns dados")
+var nome=prompt("Informe seu nome")
+var idade=prompt("Informe sua idade")
+confirm("Sua idade é "+idade+"?")
+alert("Nome: "+nome+" Idade: "+idade)
