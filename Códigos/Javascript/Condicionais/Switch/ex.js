ef86af2c486a4f40f77a4e741e88f279560e9688
Igor <igor.torquato.20@gmail.com>
2022-07-30 12:14:06 -0300

    Novos Arquivos

---
+++ Códigos/Javascript/Condicionais/Switch/ex.js
@@ -0,0 +1,12 @@
+let velocidade=80;
+
+switch(velocidade*2){
+    case 100:
+        console.log("Velocidade é 100km/s");
+        break;
+    case 160:
+        console.log("Velocidade é 160Km/s");
+        break;
+    default:
+        console.log("Velocidade não identificada!");
+}
